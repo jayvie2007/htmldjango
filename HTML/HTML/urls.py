32b4a1ab95a04e2d60ef011793f20d24d34b871b
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-03-27 10:19:37 +0800

    added functions

--- HTML/HTML/urls.py
+++ HTML/HTML/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("htmldjango.urls"))
+    path('hello/', include("htmldjango.urls")),
+    path('newyear/', include("newyear.urls"))
 ]
--- HTML/htmldjango/views.py
+++ HTML/htmldjango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('hilaaw wolrd')
+    return render(request, "htmldjango/index.html")
 
 def brian(request):
     return HttpResponse("hello, brian")
@@ -11,4 +11,6 @@ def harry(request):
     return HttpResponse("hello, harry")
 
 def greet(request, name):
-    return HttpResponse(f"Hello {name}")
+    return render(request, "htmldjango/greet.html",{
+        "name": name.capitalize()
+    })
